5a75e52ba508e9862385f08f4613533f209d4e3a
Litvinov Alexander <bumble-bee83@mail.ru>
2018-09-19 22:58:29 +0300

    additional_5: exec task not all

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let chars = str.split(''),
+        stack = [],
+        open = [],
+        close = [],
+        closeIndex,
+        openIndex;
+
+  for (let i = 0; i < bracketsConfig.length; i++){
+    open.push(bracketsConfig[i][0]);
+    close.push(bracketsConfig[i][1]);
+  }
+    
+  for (let i = 0; i < chars.length; i++) {
+    openIndex = open.indexOf(chars[i]);
+    if (openIndex !== -1) {           
+      stack.push(openIndex);
+      continue;
+    }
+
+    closeIndex = close.indexOf(chars[i]);
+    if (closeIndex !== -1) {          
+      openIndex = stack.pop();
+      if (closeIndex !== openIndex) {
+        return false;
+      }
+    }
+  }
+
+    
+  if (stack.length !== 0) {
+    return false;
+  }
+
+  return true;
 }
